6376a8d749289c0bf8c8973deda4ea5c5b17608c
Padmaprasath <padmaprasath33@gmail.com>
2023-12-12 09:50:43 +0530

    Added datadog module

--- variables.tf
+++ variables.tf
@@ -7,13 +7,3 @@ variable "aws_account_id" {
   description = "AWS account ID"
   //default     = ""
 }
-
-variable "datadog_api_key" {
-  description = "Datadog Api key"
-  default     = "7f14916484a1f8410026a47e2c2f97c6"
-}
-
-variable "datadog_app_key" {
-  description = "Datadog App key"
-  default     = "e168ad50d11be536078cb9861f7a0351cc9db0ee"
-}
